e38b773c6676ade635ad4a6199be9c20b7549bf4
Perturabo4 <perturabo40@gmail.com>
2019-10-08 15:54:55 +0300

    Add animation

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,4 +1,5 @@
 import React, {useContext} from 'react'
+import {CSSTransition} from 'react-transition-group'
 import AlertContext from '../context/alert/alertContext';
 
 
@@ -6,21 +7,28 @@ const Alert = () => {
 
     const {alert, hide} = useContext(AlertContext)
 
-    if(! alert.visible) return null;
-
     return (
-        <div className={`alert alert-${alert.type || 'warning'} alert-dismissible fade show" role="alert"`}>
-            {alert.text}
-            <button 
-                onClick={hide}
-                type="button" 
-                className="close" 
-                data-dismiss="alert" 
-                aria-label="Close"
-            >
-                <span aria-hidden="true">&times;</span>
-            </button>
-        </div>
+        <CSSTransition
+            in={alert.visible}
+            timeout={750}
+            classNames={'alert'}
+            mountOnEnter
+            unmountOnExit
+        >
+            <div className={`alert alert-${alert.type || 'warning'} alert-dismissible fade show" role="alert"`}>
+                {alert.text}
+                <button 
+                    onClick={hide}
+                    type="button" 
+                    className="close" 
+                    data-dismiss="alert" 
+                    aria-label="Close"
+                >
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        </CSSTransition>
+        
     )
 }
 
